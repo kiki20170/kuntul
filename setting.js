b6c3099e72a1a45ec00b3719f568519c1937f08b
kiki20170 <dianaffrian@gmail.com>
2024-10-03 21:28:54 +0700

    Fonts

---
+++ setting.js
@@ -0,0 +1,36 @@
+module.exports = {
+  name: "Thomz",
+  version: "4.1.0",
+  number: "",
+  owner: {
+    name: "Thomvelz",
+    number: "6289603693479",
+    whatsapp: "6289603693479@s.whatsapp.net",
+    instagram: "https://instagram.com/thomvelz_real",
+  },
+  author: {
+    name: "Thomvelz",
+    number: "6289603693479",
+    whatsapp: "6289603693479@s.whatsapp.net",
+    instagram: "https://instagram.com/thomvelz_real",
+  },
+  features: {
+    antiCall: {
+      status: true,
+      block: false,
+    },
+    selfMode: true,
+    broadcast: {
+      text: "",
+      limit: 9999,
+      jeda: 7000,
+      filterContact: false,
+    },
+    pushContacts: {
+      text: "",
+      limit: 9999,
+      jeda: 7000,
+      filterContacts: false,
+    },
+  },
+};
